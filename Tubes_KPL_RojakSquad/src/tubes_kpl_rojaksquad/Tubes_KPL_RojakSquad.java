5bfd87c5c7ab6d2cdc636b836cc93cb933f1b150
razqyaqil <razqyaqil2@gmail.com>
2022-03-25 20:22:47 +0700

    commit awal tubes

---
+++ Tubes_KPL_RojakSquad/src/tubes_kpl_rojaksquad/Tubes_KPL_RojakSquad.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tubes_kpl_rojaksquad;
+
+/**
+ *
+ * @author razqy
+ */
+public class Tubes_KPL_RojakSquad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
