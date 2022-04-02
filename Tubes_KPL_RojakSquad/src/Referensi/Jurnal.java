c8f513061e67e47984f7cd97c7dd596d4b3317ba
razqyaqil <razqyaqil2@gmail.com>
2022-04-02 21:55:34 +0700

    commit test 2, atribut dan method jurnal

--- Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
+++ Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
@@ -1,5 +1,12 @@
 package Referensi;
 
 public class Jurnal {
-    
+    private String author;
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public String getAuthor(){
+        return this.author;
+    }
 }
