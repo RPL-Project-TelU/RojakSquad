c6e4b774580a8b4f7ff2acece997478b8581de7f
ismiwrdnta <ismiwrdnta@gmail.com>
2022-04-04 00:38:26 +0800

    menambah constructor jurnal

--- Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
+++ Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
@@ -1,5 +1,11 @@
 package Referensi;
 
 public class Jurnal extends referensi {
-  
+  public Jurnal(String judul, String penulis, String kategori, int halaman, String tanggal){
+	  super(judul);
+	  super(penulis);
+	  super(kategori);
+	  super(halaman);
+	  super(tanggal);
+  }
 }
