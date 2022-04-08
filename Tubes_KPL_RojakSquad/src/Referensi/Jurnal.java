c06cde7eda5e806974e43496636a5f0d04247e1a
ismiwrdnta <ismiwrdnta@gmail.com>
2022-04-09 00:58:52 +0800

    menambah teknik defensive pada class referensi

--- Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
+++ Tubes_KPL_RojakSquad/src/Referensi/Jurnal.java
@@ -8,30 +8,21 @@ public class Jurnal extends referensi {
 
 	  private int noJurnal;
 
-	  public Jurnal(String judul, String penulis, String topik, String jenis, int noJurnal){
-	  	super(judul,penulis,topik,jenis);       
-  		this.noJurnal = noJurnal;
-
-	  }
-    
-
+      public Jurnal(String judul, String penulis, String topik, String jenis, int noJurnal) throws IllegalArgumentException {
+	        super(judul,penulis,topik,jenis); 
+            if (noJurnal > 1000000 || noJurnal < 9999999){
+                throw new IllegalArgumentException("Nomor jurnal tidak boleh lebih/kurang dari 7 digit!");
+            }else if(noJurnal == 0){
+                throw new IllegalArgumentException("Nomor jurnal tidak boleh kosong!");
+            }
+            this.noJurnal = noJurnal;      
+      }
         
-    public void setNoJurnal(int noJurnal){
-        try {
-          if (noJurnal < 1000000 || noJurnal > 9999999);
-          System.out.println("Nomor jurnal tidak boleh kurang/lebih dari 7 digit!"); 
-          this.noJurnal = noJurnal;
-        }
-        catch (Exception e) {
-          System.out.println(e.getMessage());
-        }
-            
-        }
+      public void setNoJurnal(int noJurnal){
+            this.noJurnal = noJurnal;
+      }
 
-    public int getNoJurnal(){
-      return this.noJurnal;
-    }
-
-	  
-
+      public int getNoJurnal(){
+            return this.noJurnal;
+      }		  
 }
