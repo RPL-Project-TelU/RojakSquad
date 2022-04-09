521e879d89ffbdc1566e3b42a8e64189157f2326
ngangong <selsa844@gmail.com>
2022-04-09 10:49:55 +0700

    Menambahkan Class Mahasiswa

---
+++ Tubes_KPL_RojakSquad/src/tubes_kpl_rojaksquad/Mahasiswa.java
@@ -0,0 +1,8 @@
+package fiturlogin;
+
+public class Mahasiswa {
+    String user,pass;
+    public static void Mahasiswa(String user, String pass){
+        System.out.println("Halo "+user+" Kamu login Sebagai Mahasiswa");
+    }
+}
