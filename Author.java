cc29d7160d7bb8a0eb03a0046a3d03fa117e5eb6
ngangong <selsa844@gmail.com>
2022-04-09 10:23:25 +0700

    Menambahkan Class Author

---
+++ Author.java
@@ -0,0 +1,7 @@
+package fiturlogin;
+
+public class Author extends Mahasiswa {
+    public static void Author(String user, String pass){
+        System.out.println("Halo "+user+" Kamu Login Sebagai Author");   
+    }
+}
