c79696e0b25a2aeb7337263123451d4767b66343
ngangong <selsa844@gmail.com>
2022-04-09 10:33:17 +0700

    Menambahkan Class Mahasiswa

--- Author.java
+++
@@ -1,7 +0,0 @@
-package fiturlogin;
-
-public class Author extends Mahasiswa {
-    public static void Author(String user, String pass){
-        System.out.println("Halo "+user+" Kamu Login Sebagai Author");   
-    }
-}
