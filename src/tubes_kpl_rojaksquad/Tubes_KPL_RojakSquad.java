11d66d13418855d0358af7a6e99e3af0877fec47
Kevin Febrian <pownyfals@gmail.com>
2022-04-10 13:09:26 +0700

    memindahkan semua file keluar folder

---
+++ src/tubes_kpl_rojaksquad/Tubes_KPL_RojakSquad.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tubes_kpl_rojaksquad;
+import fitur_searching.Searching;
+import java.io.IOException;
+/**
+ *
+ * @author razqy
+ */
+public class Tubes_KPL_RojakSquad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        Searching s = new Searching();
+        s.cariBuku("Rekayasa");
+    }
+    
+}
