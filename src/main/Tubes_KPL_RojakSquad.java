b965eab727e6b1d8d1dc328b29536d8e97d3a22e
Kevin Febrian <pownyfals@gmail.com>
2022-04-16 21:28:29 +0700

    Update Tubes_KPL_RojakSquad.java

--- src/main/Tubes_KPL_RojakSquad.java
+++ src/main/Tubes_KPL_RojakSquad.java
@@ -3,10 +3,10 @@ import fitur_login.*;
 import fitur_searching.*;
 import fitur_referensi.*;
 import java.io.IOException;
-/**
- *
- * @author razqy
- */
+  
+//PADA FOLDER SOURCE CODE ADA FOLDER "jar files" YANG BERISIKAN 4 FILE JAR SEBELUM MEMULAI PROGRAM,
+//MASUKKAN DAHULU KEEMPAT FILE KEDALAM PROJECT LIBRARY DI BAGIAN KIRI KLIK KANAN "Libraries" DAN PILIH "Add Jar/Folder" DAN MASUKKAN 4 FILE TERSEBUT
+
 public class Tubes_KPL_RojakSquad {
 
     /**
