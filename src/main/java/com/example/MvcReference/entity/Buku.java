6a7b00ba7be8e51edb46bb9f51a24cb9813b0483
Pownyfals <pownyfals@gmail.com>
2022-06-17 21:25:07 +0700

    ui detail buku

--- src/main/java/com/example/MvcReference/entity/Buku.java
+++ src/main/java/com/example/MvcReference/entity/Buku.java
@@ -9,6 +9,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table
+/**
+ * menyimpan id buku, judul buku, penulis buku, penerbit buku
+ * id buku digenerate automatis sebagai primary key.
+ */
 public class Buku {
     @Id
     @SequenceGenerator(name = "buku_sequence", sequenceName = "buku_sequence", allocationSize = 1)
@@ -18,15 +22,32 @@ public class Buku {
     private String penulis;
     private String penerbit;
 
+
+    /**
+     * 
+     * @param judul berisikan String bebas
+     * @param penulis berisikan String bebas
+     * @param penerbit berisikan String bebas
+     */
     public Buku(String judul, String penulis, String penerbit) {
         this.judul = judul;
         this.penulis = penulis;
         this.penerbit = penerbit;
     }
-
+    /**
+     * 
+     * Constructor default
+     */
     public Buku() {
     }
 
+    /**
+     * 
+     * @param id berisi Long dengan angka bebas
+     * @param judul berisikan String bebas
+     * @param penulis berisikan String bebas
+     * @param penerbit berisikan String bebas
+     */
     public Buku(Long id, String judul, String penulis, String penerbit) {
         this.id = id;
         this.judul = judul;
@@ -34,41 +55,68 @@ public class Buku {
         this.penerbit = penerbit;
     }
 
+    /**
+     * @return Long dari values id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * 
+     * @param id berisikan primary key buku
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * 
+     * @return string dari value judul
+     */
     public String getJudul() {
         return judul;
     }
 
+    /**
+     * 
+     * @param judul berisi String judul dari buku
+     */
     public void setJudul(String judul) {
         this.judul = judul;
     }
 
+    /**
+     * 
+     * @return string dari value penulis
+     */
     public String getPenulis() {
         return penulis;
     }
 
+    /**
+     * 
+     * @param penulis berisikan String penulis buku
+     */
     public void setPenulis(String penulis) {
         this.penulis = penulis;
     }
 
+    /**
+     * 
+     * @return String dari values penerbit
+     */
     public String getPenerbit() {
         return penerbit;
     }
 
+    /**
+     * 
+     * @param penerbit berisikan String penerbit dari buku
+     */
     public void setPenerbit(String penerbit) {
         this.penerbit = penerbit;
     }
 
-    @Override
-    public String toString() {
-        return "Buku [id=" + id + ",\n\n judul=" + judul + ",\n\n penerbit=" + penerbit + ",\n\n penulis=" + penulis
-                + "]";
-    }
-}
+
+}   
