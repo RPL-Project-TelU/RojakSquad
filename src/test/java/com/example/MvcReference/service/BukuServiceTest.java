630d7be9b42f57488312cf58970de108898b8470
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2022-06-18 17:07:21 +0700

    added unit test

--- src/test/java/com/example/MvcReference/service/BukuServiceTest.java
+++ src/test/java/com/example/MvcReference/service/BukuServiceTest.java
@@ -1,6 +1,10 @@
 package com.example.MvcReference.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +22,28 @@ public class BukuServiceTest {
     @Test
     void testFindBuku() {
         Buku buku1 = new Buku("test", "dadang", "terbit");
+        Buku buku2 = new Buku("testing", "dadang", "terbit");
         underTest.save(buku1);
+        underTest.save(buku2);
 
         assertEquals(buku1, underTest.findBuku("test"));
+        assertNotEquals(buku2, underTest.findBuku("test"));
 
     }
+
+    @Test
+    void testSearchBuku() {
+        List<Buku> buku = new ArrayList<Buku>();
+        Buku buku1 = new Buku("test", "dadang", "terbit");
+        Buku buku2 = new Buku("testing", "dadang", "terbit");
+        buku.add(buku1);
+        buku.add(buku2);
+
+        underTest.saveAll(buku);
+
+        assertEquals(buku, underTest.searchBukuByJudul("test"));
+    }
+
+
+
 }
