844e51d1fab3a9629f5ce595ab874cabf9f8ed52
Pownyfals <pownyfals@gmail.com>
2022-06-29 16:15:55 +0700

    improve buku.html UI

--- src/main/java/com/example/MvcReference/controller/ViewController.java
+++ src/main/java/com/example/MvcReference/controller/ViewController.java
@@ -2,18 +2,13 @@ package com.example.MvcReference.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,7 +16,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.MvcReference.entity.Buku;
 import com.example.MvcReference.entity.Users;
 import com.example.MvcReference.service.BukuServiceImpl;
-import com.example.MvcReference.service.UserService;
 import com.example.MvcReference.service.UserServiceImpl;
 import com.example.MvcReference.util.FileUploadUtil;
 
